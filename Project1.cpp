320fb9ac555291038ab52622c84a72df479c0e15
KonstantinAvdeenko <AvdeenkoKonstantin1995@gmail.com>
2016-06-11 18:58:56 +0400

    Add my finding the product of even elements of the array are not multiples of ten

---
+++ Project1.cpp
@@ -0,0 +1,32 @@
+﻿#include <tchar.h>
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int _tmain(int argc, wchar_t argv[]) // функция поддержки юникода и остальных символов
+{
+	srand(time(NULL)); // генератор псевдослучайных чисел зависящий от временной даты
+	setlocale(LC_ALL, "Russian"); // поддержка работы с русскими символами
+	int arr[30];
+	for (int i = 0; i < 30; i++)
+	{
+		arr[i] = rand() % 100;
+		cout << " " << arr[i]; // заполнение и вывод элементов массива с рандомными числами от 0 до 100
+	}
+	int pr = 1;
+	for (int j = 0; j<30; j++)
+	{
+		if (arr[j] % 2 == 0)
+		{
+			if (arr[j] % 10 != 0)
+			{ 
+				 pr = pr*arr[j];  
+			}
+		}
+	}
+	cout << endl;
+	cout << "Произведение четных элементов массива, не кратных 10 равно " << pr;
+	cin.get(); // окончание работы с потоками
+	return 0;
+}
---
+++ READ_ME_ENG.md
@@ -0,0 +1,4 @@
+﻿This program in the C++ programming language, finds the product of even elements of an array,
+not multiples of ten. The result is output through the output stream.
+The main module Project1 contains a single main function:
+_tmain - fully performing the above task.
